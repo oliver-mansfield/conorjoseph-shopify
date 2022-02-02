e7f84e9f9993947892fecc51b5c07e6357d90b27
omansfield <omansfield.dev@gmail.com>
2022-02-02 11:47:30 +0000

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	env: {
+		SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+		SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+	},
+
+	reactStrictMode: true,
+
+	images: {
+		domains: ["cdn.shopify.com"],
+	},
+};
---
+++ pages/productlisting.js
@@ -0,0 +1,25 @@
+import { getAllProducts } from "../lib/shopify";
+
+export default function ProductListing({ products }) {
+	return (
+		<div>
+			<h2>Listing</h2>
+			<div>
+				{products.map((product) => (
+					<div key={product.node.id}>
+						<p>{product.node.title}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export async function getStaticProps() {
+	const products = await getAllProducts();
+	console.log(products);
+
+	return {
+		props: { products }, // will be passed to the page component as props
+	};
+}
