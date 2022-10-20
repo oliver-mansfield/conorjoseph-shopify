5bc800edee84bcf0dda4bf830a999af841fa28e0
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-10-20 23:16:42 +0100

    Working fade out of nonclicked product items

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,14 +1,61 @@
 import ProductListItem from "./ProductListItem";
+import {useRef, useState} from "react";
+import gsap from "gsap/dist/all";
 
 const ProductList = ({products}) => {
+	const productItemsRef = useRef([]);
+
+	const handleClick = (product, index) => {
+		// const url = `/products/${product.node.handle}`;
+
+		//Create an array of indexes of all the items except the one which was clicked (index)
+		const arrayOfIndexes = [];
+
+		for (let i = 0; i < productItemsRef.current.length; i++) {
+			if (i !== index) {
+				arrayOfIndexes.push(i);
+			}
+			console.log(arrayOfIndexes);
+		}
+
+		for (let i = 0; i < arrayOfIndexes.length; i++) {
+			console.log(productItemsRef.current[arrayOfIndexes[i]]);
+
+			gsap.timeline().to(productItemsRef.current[arrayOfIndexes[i]], {
+				duration: 1.5,
+				opacity: 0,
+			});
+		}
+	};
+
 	return (
 		<div className="container grid grid-cols-1 md:grid-cols-2 md:gap-x-8 md:gap-y-16">
 			{products.map((product, index) => (
-				<div key={product.node.id}>
-					<button onClick={() => productItemRefs.current[index].startAnimate()}>
-						Click
+				<div
+					key={product.node.id}
+					ref={(el) => {
+						productItemsRef.current.push(el);
+					}}
+				>
+					<button
+						onClick={() => {
+							handleClick(product, index);
+						}}
+						className="cursor-pointer"
+					>
+						<div>
+							<img
+								src={product.node.images.edges[0].node.originalSrc}
+								// ref={imageRef}
+							/>
+
+							<div className="mt-[-3rem] text-center perspective-container">
+								<h2 className="perspective-text">{product.node.title}</h2>
+								{/* <p>£{product.node.priceRange.minVariantPrice.amount}</p> */}
+							</div>
+						</div>
 					</button>
-					<ProductListItem product={product} />
+					{/* <ProductListItem product={product} /> */}
 				</div>
 			))}
 		</div>
--- components/home/Splash.js
+++ components/home/Splash.js
@@ -1,8 +1,6 @@
 const Splash = (imgUrl) => {
 	return (
-		<div className="splash fixed top-0 left-0 w-full h-full pointer-events-none overflow-hidden">
-			<p>SPLASH</p>
-		</div>
+		<div className="splash fixed top-0 left-0 w-f pointer-events-none overflow-hidden"></div>
 	);
 };
 
