de546ed2b5255dc37cf159d122578db0cc84efce
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-11-01 09:08:02 +0000

    mobile sizes for cover. gsap needs media query

--- components/home/Cover.js
+++ components/home/Cover.js
@@ -6,27 +6,41 @@ const Cover = () => {
 	const lineRef = useRef(null);
 	const conorRef = useRef(null);
 	const skinRef = useRef(null);
+	const girlRef = useRef(null);
+	const guyRef = useRef(null);
 
 	useEffect(() => {
-		gsap.timeline().from(lineRef.current, {
-			width: "0px",
-			duration: 1,
-			ease: "power3.inOut",
-			delay: 1.75,
-		});
-
 		gsap.timeline().from(conorRef.current, {
 			y: "48px",
 			duration: 1.5,
 			ease: "power3.inOut",
 			delay: 1,
 		});
-
 		gsap.timeline().from(skinRef.current, {
 			y: "-50px",
 			duration: 1.5,
 			ease: "power3.inOut",
 			delay: 1,
+		});
+		gsap.timeline().from(girlRef.current, {
+			x: "30px",
+			opacity: 0,
+			duration: 1.5,
+			ease: "power3.inOut",
+			delay: 1,
+		});
+		gsap.timeline().from(guyRef.current, {
+			x: "-30px",
+			opacity: 0,
+			duration: 1.5,
+			ease: "power3.inOut",
+			delay: 1,
+		});
+		gsap.timeline().from(lineRef.current, {
+			width: "0px",
+			duration: 1,
+			ease: "power3.inOut",
+			delay: 2,
 		});
 	}, []);
 
@@ -34,17 +48,24 @@ const Cover = () => {
 		<>
 			<div className="gradient-bg-cover h-screen">
 				<div className="relative h-screen overflow-hidden">
-					<div className="absolute z-0 w-full bottom-1/2 flex justify-center overflow-hidden pb-5">
-						<h1 ref={conorRef}>CONOR JOSEPH</h1>
-					</div>
-					<div className="absolute z-0 w-full top-1/2 flex justify-center">
-						<hr className="w-full" ref={lineRef} />
-					</div>
-					<div className="absolute z-0 w-full top-1/2 flex justify-center overflow-hidden pt-5">
-						<h1 ref={skinRef}>SECOND SKIN</h1>
+					<div className="absolute z-0 h-full w-full top-32 left-0 sm:top-0 sm:flex items-center justify-center">
+						<div className="relative w-full">
+							<div className="absolute w-full bottom-1/2 flex justify-center overflow-hidden pb-5">
+								<h1 ref={conorRef}>CONOR JOSEPH</h1>
+							</div>
+							<div className="absolute w-full top-1/2 flex justify-center">
+								<hr className="w-full" ref={lineRef} />
+							</div>
+							<div className="absolute w-full top-1/2 flex justify-center overflow-hidden pt-5">
+								<h1 ref={skinRef}>SECOND SKIN</h1>
+							</div>
+						</div>
 					</div>
 
-					<div className="w-[400px] sm:w-[500px] absolute left-[-110px] sm:left-0 bottom-0 z-10">
+					<div
+						className="w-[345px] sm:w-[400px] lg:w-[500px] absolute left-[-110px] lg:left-[-100px] xl:left-0 bottom-0 z-10"
+						ref={girlRef}
+					>
 						<Image
 							src="/images/girl.png"
 							layout="responsive"
@@ -52,7 +73,10 @@ const Cover = () => {
 							height="1000"
 						/>
 					</div>
-					<div className="w-[400px] sm:w-[500px] absolute right-[-110px] sm:right-0 bottom-0">
+					<div
+						className="w-[345px] sm:w-[400px] lg:w-[500px] absolute right-[-110px] lg:right-[-100px] xl:right-0 bottom-0"
+						ref={guyRef}
+					>
 						<Image
 							src="/images/guy.png"
 							layout="responsive"
--- components/home/Intro.js
+++ components/home/Intro.js
@@ -1,7 +1,21 @@
 const Intro = () => {
 	return (
 		<>
-			<p>This is the intro</p>
+			<div className="max-w-2xl grid grid-cols sm:grid-cols-2 gap-5 mx-auto ">
+				<p>
+					Second Skin is the latest collection by Conor Joseph, which sees the
+					Jewellery artist pay homage to the human body and its varying
+					differences and uniqueness.
+				</p>
+				<p>
+					The collection is made of 30 pieces, all made on fine gold plated
+					silver, hand crafted in London’s prestigious Hatton Garden. Like most
+					of Conor’s work, Second Skin is a dark and obscure collection, yet his
+					use of often-uncelebrated details of the skin such as wrinkles,
+					freckles and scarring creates provokes feelings of intimacy and
+					poeticism.
+				</p>
+			</div>
 		</>
 	);
 };
