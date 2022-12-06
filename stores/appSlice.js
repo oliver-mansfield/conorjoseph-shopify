33f5a73d467082c5ebe2b1c4493c6e13a31021c7
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-12-06 09:12:12 +0000

    Remove unused transition var

--- stores/appSlice.js
+++ stores/appSlice.js
@@ -2,7 +2,6 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
 	splashVisible: false,
-	// isTransitioning: false,
 };
 
 export const appSlice = createSlice({
@@ -16,22 +15,10 @@ export const appSlice = createSlice({
 			console.log("hide splash");
 			state.splashVisible = false;
 		},
-		// startPageTransition: (state) => {
-		// 	state.isTransitioning = true;
-		// },
-		// endPageTransition: (state) => {
-		// 	state.isTransitioning = false;
-		// },
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const {
-	splashVisible,
-	showSplash,
-	hideSplash,
-	startPageTransition,
-	endPageTransition,
-} = appSlice.actions;
+export const {splashVisible, showSplash, hideSplash} = appSlice.actions;
 
 export default appSlice.reducer;
