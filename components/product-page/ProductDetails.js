e43711f5b6a50c3e6c68407bd6168e42d8e3bedd
oliver-mansfield <olivermansfield.dev@gmail.com>
2023-04-16 12:01:29 +0100

    glass prod describption maybe remove

--- components/product-page/ProductDetails.js
+++ components/product-page/ProductDetails.js
@@ -17,24 +17,26 @@ const ProductDetails = ({ productData }) => {
 
 	return (
 
-		<div
-			className="max-w-4xl grid grid-cols-5 gap-4 mx-auto px-4 rounded-md z-20 relative"
-			ref={detailsRef}
-		>
-			<div className="col-start-1 col-end-6 md:col-start-1 md:col-end-3">
-				<h2 className="text-center md:text-right">{productData.title}</h2>
-				<h3 className="text-center md:text-right">
-					{formatPrice(productData.priceRange.minVariantPrice.amount)}
-				</h3>
-			</div>
-
-			<div className="col-start-1 col-end-6 md:col-start-3 md:col-end-6">
-				<p className="font-sansSerif text-center md:text-left">{productData.description}</p>
-				<div className="flex justify-center md:justify-start mt-8">
-					<AddToCartButton productData={productData} />
+		<div className="md:fixed bottom-4 w-full z-10 flex justify-center">
+			<div
+				className="max-w-4xl grid grid-cols-5 gap-4 px-4 rounded-md z-20 backdrop-filter-glass p-4 mx-4"
+				ref={detailsRef}
+			>
+				<div className="col-start-1 col-end-6 md:col-start-1 md:col-end-3">
+					<h2 className="text-center md:text-right">{productData.title}</h2>
+					<h3 className="text-center md:text-right">
+						{formatPrice(productData.priceRange.minVariantPrice.amount)}
+					</h3>
 				</div>
-			</div>
 
+				<div className="col-start-1 col-end-6 md:col-start-3 md:col-end-6">
+					<p className="font-sansSerif text-center md:text-left">{productData.description}</p>
+					<div className="flex justify-center md:justify-start mt-8">
+						<AddToCartButton productData={productData} />
+					</div>
+				</div>
+
+			</div>
 		</div>
 
 	);
